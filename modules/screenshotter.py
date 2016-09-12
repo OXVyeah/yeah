7e36042cf547b98daff004dca328b1087a459ba6
OXVyeah <2529004225@qq.com>
2016-09-12 16:41:46 +0800

    Update screenshotter.py

--- modules/screenshotter.py
+++ modules/screenshotter.py
@@ -7,24 +7,29 @@ def run(**args):
     # grab a handle to the main desktop window
     hdesktop = win32gui.GetDesktopWindow()
 
+    print("1")
     # determine the size of all monitors in pixels
     width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
     height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
     left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
     top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
 
+    print("2")
     # create a device context
     desktop_dc = win32gui.GetWindowDC(hdesktop)
     img_dc = win32ui.CreateDCFromHandle(desktop_dc)
 
+    print("3")
     # create a memory based device context
     mem_dc = img_dc.CreateCompatibleDC()
 
+    print("4")
     # create a bitmap object
     screenshot = win32ui.CreateBitmap()
     screenshot.CreateCompatibleBitmap(img_dc, width, height)
     mem_dc.SelectObject(screenshot)
 
+    print("5")
     # copy the screen into our memory device context
     mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
 
